58d63bd8b1d3fefc9a8cf810e883db13510d1332
Lieke <arisuthirty427@gmail.com>
2025-11-03 15:16:53 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ set -e
 
 # Install required packages
 echo "[*] Installing required packages..."
-sudo pacman -S --noconfirm hyprland xorg-xrandr rust firefox noto-fonts-cjk noto-fonts-emoji noto-fonts fastfetch pacman-contrib hyprshot waybar hyprpaper rofi sddm nwg-look kitty nemo hyprpolkitagent pipewire-pulse git xdg-desktop-portal-hyprland git noto-fonts breeze-gtk
+sudo pacman -S --noconfirm hyprland xorg-xrandr rust firefox noto-fonts-cjk noto-fonts-emoji noto-fonts fastfetch qt6ct pacman-contrib hyprshot waybar hyprpaper rofi sddm nwg-look kitty nemo hyprpolkitagent pipewire-pulse git xdg-desktop-portal-hyprland git noto-fonts breeze-gtk
 
 # Clone dotfiles 
 DOTFILES_DIR="$HOME/.dotfiles"
